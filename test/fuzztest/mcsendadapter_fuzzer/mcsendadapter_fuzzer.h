e1502bb7fec7541736f3e9445bbdb145431125a4
W_Qiye <wangzhiqian3@huawei.com>
2026-06-04 17:12:53 +0800

    update: 更新文件 mcsendadapter_fuzzer.h
    Signed-off-by: W_Qiye <wangzhiqian3@huawei.com>

--- test/fuzztest/mcsendadapter_fuzzer/mcsendadapter_fuzzer.h
+++ test/fuzztest/mcsendadapter_fuzzer/mcsendadapter_fuzzer.h
@@ -16,4 +16,72 @@
 #ifndef MC_SEND_ADAPTER_FUZZER_H
 #define MC_SEND_ADAPTER_FUZZER_H
 
+#include <memory>
+#include <cstdint>
+
+#include "mc_send_adapter.h"
+#include "mc_command_base.h"
+#include "mc_data_buffer.h"
+
+namespace OHOS {
+namespace MechBodyController {
+
+class MockCommand : public CommandBase {
+public:
+    MockCommand() = default;
+    ~MockCommand() override = default;
+
+    std::shared_ptr<MechDataBuffer> Marshal() const override
+    {
+        auto buffer = std::make_shared<MechDataBuffer>(32);
+        return buffer;
+    }
+
+    void TriggerResponse(std::shared_ptr<MechDataBuffer> data) override
+    {
+        responseTriggered_ = true;
+    }
+
+    void SetCmdSet(uint8_t cmdSet)
+    {
+        cmdSet_ = cmdSet;
+    }
+
+    void SetCmdId(uint8_t cmdId)
+    {
+        cmdId_ = cmdId;
+    }
+
+    void SetReqSize(uint16_t reqSize)
+    {
+        reqSize_ = reqSize;
+    }
+
+    void SetRspSize(uint16_t rspSize)
+    {
+        rspSize_ = rspSize;
+    }
+
+    void SetNeedResponse(bool need)
+    {
+        needResponse_ = need;
+    }
+
+    void SetRetryTimes(int32_t times)
+    {
+        retryTimes_ = times;
+    }
+
+    void SetTimeoutMs(uint32_t timeout)
+    {
+        timeoutMs_ = timeout;
+    }
+
+private:
+    bool responseTriggered_ = false;
+};
+
+} // namespace MechBodyController
+} // namespace OHOS
+
 #endif // MC_SEND_ADAPTER_FUZZER_H
