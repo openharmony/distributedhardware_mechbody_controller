f16fe32968cc99bfe96d5a0668d9e98a8466828e
huyiming1236 <huxiaoyu12@h-partners.com>
2026-06-12 17:07:57 +0800

    update: 更新文件 resource_manager_utils.cpp
    Signed-off-by: huyiming1236 <huxiaoyu12@h-partners.com>

--- services/src/utils/resource_manager_utils.cpp
+++ services/src/utils/resource_manager_utils.cpp
@@ -16,52 +16,94 @@
 #include "resource_manager_utils.h"
 
 #include "mechbody_controller_log.h"
+#include "locale_config.h"
 
 namespace OHOS {
+namespace MechBodyController {
 namespace {
     const std::string TAG = "ResourceManagerUtils";
 }
 using namespace Global::Resource;
 
+const char *HAP_PATH = "/system/app/DHardware_UI/DHardware_UI.hap";
+
 std::mutex ResourceManagerUtils::resourceManagerMutex_;
 
 void ResourceManagerUtils::InitResourceManager()
 {
-    if (!ResourceManagerUtils::resourceManager_) {
-        HILOGI("Init");
-        ResourceManagerUtils::resourceManager_ = Global::Resource::GetSystemResourceManagerNoSandBox();
+    if (!resConfig_) {
+        resConfig_ = Global::Resource::CreateResConfig();
     }
+    if (!resourceManager_) {
+        HILOGI("Init");
+        resourceManager_ = Global::Resource::CreateResourceManager(false);
+        if (resourceManager_ == nullptr) {
+            HILOGE("resourceManager new failed");
+            return;
+        }
+        if (!resourceManager_->AddResource(HAP_PATH)) {
+            HILOGE("AddResource failed");
+            return;
+        }
+    }
+    RefreshResConfig();
+}
+
+void ResourceManagerUtils::RefreshResConfig()
+{
+    std::string language = Global::I18n::LocaleConfig::GetSystemLanguage();
+    if (language.empty()) {
+        HILOGE("Get system language failed, skip RefreshResConfig");
+        return;
+    }
+    UErrorCode status = U_ZERO_ERROR;
+    icu::Locale locale = icu::Locale::forLanguageTag(language, status);
+    if (status != U_ZERO_ERROR || locale == nullptr) {
+        HILOGE("forLanguageTag failed, errCode:%{public}d", status);
+        return;
+    }
+    if (!resConfig_) {
+        HILOGE("resConfig_ is nullptr");
+        return;
+    }
+    resConfig_->SetLocaleInfo(locale.getLanguage(), locale.getScript(), locale.getCountry());
+    if (!resourceManager_) {
+        HILOGE("resourceManager_ is nullptr");
+        return;
+    }
+    resourceManager_->UpdateResConfig(*resConfig_);
+    HILOGI("Refresh success");
 }
 
 std::string ResourceManagerUtils::GetSystemStringByName(std::string name)
 {
-    std::lock_guard<std::mutex> lock(ResourceManagerUtils::resourceManagerMutex_);
-    ResourceManagerUtils::InitResourceManager();
+    std::lock_guard<std::mutex> lock(resourceManagerMutex_);
+    InitResourceManager();
 
     std::string result;
-    if (!ResourceManagerUtils::resourceManager_) {
+    if (!resourceManager_) {
         HILOGE("resourceManager_ is nullptr");
         return result;
     }
 
-    Global::Resource::RState rstate = ResourceManagerUtils::resourceManager_->GetStringByName(name.c_str(), result);
+    Global::Resource::RState rstate = resourceManager_->GetStringByName(name.c_str(), result);
     HILOGI("name: %{public}s, rstate: %{public}d", name.c_str(), static_cast<int32_t>(rstate));
     return result;
 }
 
 std::shared_ptr<Media::PixelMap> ResourceManagerUtils::GetPixelMapByName(std::string name)
 {
-    std::lock_guard<std::mutex> lock(ResourceManagerUtils::resourceManagerMutex_);
-    ResourceManagerUtils::InitResourceManager();
+    std::lock_guard<std::mutex> lock(resourceManagerMutex_);
+    InitResourceManager();
 
-    if (!ResourceManagerUtils::resourceManager_) {
+    if (!resourceManager_) {
         HILOGE("resourceManager_ is nullptr");
         return nullptr;
     }
 
     size_t len = 0;
     std::unique_ptr<uint8_t[]> outValue;
-    Global::Resource::RState rstate = ResourceManagerUtils::resourceManager_->GetMediaDataByName(
+    Global::Resource::RState rstate = resourceManager_->GetMediaDataByName(
         name.c_str(), len, outValue);
     HILOGI("rstate: %{public}d", static_cast<int32_t>(rstate));
 
@@ -75,11 +117,11 @@ std::shared_ptr<Media::PixelMap> ResourceManagerUtils::GetPixelMapByName(std::st
         return nullptr;
     }
     Media::DecodeOptions decodeOpts;
-    decodeOpts.desiredPixelFormat = Media::PixelFormat::BGRA_8888;
     std::unique_ptr<Media::PixelMap> pixelMap = imageSource->CreatePixelMap(decodeOpts, errorCode);
     HILOGI("CreatePixelMap errorCode: %{public}d", static_cast<int32_t>(errorCode));
 
     std::shared_ptr<Media::PixelMap> iconPixelMap = std::move(pixelMap);
     return iconPixelMap;
+}
 }
 }
